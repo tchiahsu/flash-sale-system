871c96b83c878b23f33823e4d8afa1d953215051
Tony Hsu Tai <hsutai.c@gmail.com>
2026-03-30 01:14:15 -0400

    modify deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ACCOUNT="835637956575"
+ACCOUNT="959856142011"
 REGION="us-east-1"
 CLUSTER="flash-sale-system-cluster"
 PROJECT="flash-sale-system"
@@ -8,7 +8,7 @@ aws ecr get-login-password --region $REGION | docker login --username AWS --pass
 
 for SERVICE in api-gateway order-service inventory-service notification-service; do
   IMAGE="$ACCOUNT.dkr.ecr.$REGION.amazonaws.com/$PROJECT/$SERVICE:latest"
-  docker build -t $IMAGE services/$SERVICE
+  docker build --platform linux/amd64 -t $IMAGE services/$SERVICE
   docker push $IMAGE
   aws ecs update-service --cluster $CLUSTER --service $PROJECT-$SERVICE --force-new-deployment
 done
